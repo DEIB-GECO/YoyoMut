77528817fab7adc2f2bf1d985404ce1badac7ebe
jpenic <jp010809267@fer.hr>
2025-06-12 20:32:05 +0200

    Improve protein visualization

--- pages/4_Generate_PDF_reports.py
+++ pages/4_Generate_PDF_reports.py
@@ -20,15 +20,17 @@ st.write("Input parameters you want to use for the reports:")
 with st.expander("Threshold algorithm"):
     with st.form("parameters-threshold", enter_to_submit=False):
         st.write("Please input parameters for amino acid residue classification")
-        threshold = int(st.number_input('Global relative frequency threshold (%):', value=30, placeholder='30',
-                                        help="The threshold defines the minimal proportion of sequences "
-                                             "that must contain the mutation for it to be relevant."
-                                        )) / 100
+        threshold = st.number_input('Global relative frequency threshold (0-1):', value=0.3, placeholder='0.3',
+                                    help="The threshold defines the minimal proportion of sequences "
+                                         "that must contain the mutation for it to be relevant.",
+                                    min_value=0.0,
+                                    max_value=1.0
+                                    )
 
-        min_days = int(st.number_input('Minimal duration (in days): ', value=30, placeholder='30',
-                                       help="Minimal number of days above the selected relative frequency threshold "
-                                            "to be considered significant."
-                                       ))
+        min_days = st.number_input('Minimal duration (in days): ', value=30, placeholder='30',
+                                   help="Minimal number of days above the selected relative frequency threshold "
+                                        "to be considered significant."
+                                   )
 
         submitted_threshold = st.form_submit_button("Submit")
 
@@ -40,13 +42,12 @@ with st.expander("Slope algorithm"):
                                                 " The parameter can increase or decrease sensitivity of the algorithm."))
         submitted_slope = st.form_submit_button("Submit")
 
-
 # When submit is clicked trigger new computation only if the parameter values changed.
 # Then also delete currently saved reports.
 if submitted_threshold:
     st.session_state.submitted_threshold = True
-    if st.session_state.get('threshold') != threshold \
-            or st.session_state.get('min_days') != min_days:
+    if st.session_state.get('report_threshold_value') != threshold \
+            or st.session_state.get('report_min_days_value') != min_days:
         st.session_state.threshold = threshold
         st.session_state.min_days = min_days
         if 'yo_yo_threshold_report_path' in st.session_state:
@@ -58,7 +59,7 @@ else:
 
 if submitted_slope:
     st.session_state.submitted_slope = True
-    if st.session_state.get('slope_points') != slope_points:
+    if st.session_state.get('report_slope_points_value') != slope_points:
         st.session_state.slope_points = slope_points
         if 'yo_yo_slope_report_path' in st.session_state:
             del st.session_state['yo_yo_slope_report_path']
@@ -70,7 +71,9 @@ else:
 # Running the algorithms
 if st.session_state.get("submitted_threshold"):
     st.session_state.classified_mutations_threshold = \
-        classify_mutations_threshold(st.session_state.smoothed_data_files_sequences, threshold, min_days)
+        classify_mutations_threshold(st.session_state.smoothed_data_files_sequences,
+                                     st.session_state.threshold,
+                                     st.session_state.min_days)
 
     st.session_state.yo_yo_mutations_threshold, st.session_state.fixated_mutations_threshold = \
         filter_mutations(st.session_state.classified_mutations_threshold)
@@ -97,7 +100,9 @@ if threshold_alg_computed:
                            report_name='yo_yo_mutations_threshold_analysis',
                            data_dict=st.session_state.yo_yo_mutations_threshold,
                            parameters={'Threshold': st.session_state.threshold,
-                            'Minimum duration (in days)': st.session_state.min_days})
+                                       'Minimum duration (in days)': st.session_state.min_days})
+        st.session_state.report_threshold_value = st.session_state.threshold
+        st.session_state.report_min_days_value = st.session_state.min_days
     if 'fixated_threshold_report_path' not in st.session_state:
         st.session_state.fixated_threshold_report_path = \
             results_to_PDF(report_path='./reports/',
@@ -117,16 +122,15 @@ if slope_alg_computed:
                            report_name='yo_yo_mutations_slope_analysis',
                            data_dict=st.session_state.yo_yo_mutations_slope,
                            parameters={'Time points used to calculate slope': st.session_state.slope_points})
-
+        st.session_state.report_slope_points_value = st.session_state.slope_points
     if 'fixated_slope_report_path' not in st.session_state:
         st.session_state.fixated_slope_report_path = \
             results_to_PDF(report_path='./reports/',
                            report_title='Fixated mutations',
                            additional_info='Classification of residues by prevalence slope analysis',
                            report_name='fixated_mutations_slope_analysis',
-                           data_dict=st.session_state.fixated_mutations_threshold,
+                           data_dict=st.session_state.fixated_mutations_slope,
                            parameters={'Time points used to calculate slope': st.session_state.slope_points})
-
 
 # Show buttons for PDF report download
 reports_threshold, reports_slope = st.columns(2)
@@ -169,7 +173,6 @@ with reports_threshold:
             icon=":material/download:",
         )
 
-
 with reports_slope:
     st.markdown("### Download slope algorithm results as PDFs:")
     if 'yo_yo_slope_report_path' in st.session_state:
@@ -208,4 +211,3 @@ with reports_slope:
             mime="application/pdf",
             icon=":material/download:",
         )
-
