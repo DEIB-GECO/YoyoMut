f3825aa026eeffd6439616229489ec5a32f1f6c1
jpenic <jp010809267@fer.hr>
2025-09-23 12:22:12 +0200

    Data automatization

---
+++ YoyoMut_data_pipeline.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+echo "Starting data pipeline..."
+python3 data_pipeline.py
+echo "Finished data pipeline."
+
+docker-compose down
+echo "Moving data..."
+cp data/ ../YoyoMut_data/
+echo "Finished moving data."
+docker-compose up
